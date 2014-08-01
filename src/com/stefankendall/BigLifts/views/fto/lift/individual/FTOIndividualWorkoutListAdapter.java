c775ceab50b5a712ef242a008eacf589679a6e37
Stefan Kendall <stefankendall@gmail.com>
2014-07-31 20:00:27 -0400

    Added section titles.

--- src/com/stefankendall/BigLifts/views/fto/lift/individual/FTOIndividualWorkoutListAdapter.java
+++ src/com/stefankendall/BigLifts/views/fto/lift/individual/FTOIndividualWorkoutListAdapter.java
@@ -4,8 +4,10 @@ import android.app.Activity;
 import com.google.common.collect.Lists;
 import com.stefankendall.BigLifts.data.models.fto.JFTOWorkout;
 import com.stefankendall.BigLifts.views.lists.CustomListItem;
+import com.stefankendall.BigLifts.views.lists.HeaderListItem;
 import com.stefankendall.BigLifts.views.lists.SimpleListAdapter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FTOIndividualWorkoutListAdapter extends SimpleListAdapter {
@@ -18,6 +20,30 @@ public class FTOIndividualWorkoutListAdapter extends SimpleListAdapter {
 
     @Override
     public List<? extends CustomListItem> buildItems() {
-        return Lists.newArrayList();
+        List<CustomListItem> items = Lists.newArrayList();
+
+        if (this.hasWarmup()) {
+            items.add(new HeaderListItem("Warm-up"));
+        }
+        if (this.hasWorkSets()) {
+            items.add(new HeaderListItem("Workout"));
+        }
+        if (this.hasAssistance()) {
+            items.add(new HeaderListItem("Assistance"));
+        }
+
+        return items;
+    }
+
+    protected boolean hasWarmup() {
+        return this.jftoWorkout.workout.warmupSets().size() > 0;
+    }
+
+    protected boolean hasAssistance() {
+        return this.jftoWorkout.workout.warmupSets().size() > 0;
+    }
+
+    protected boolean hasWorkSets() {
+        return this.jftoWorkout.workout.workSets().size() > 0;
     }
 }
