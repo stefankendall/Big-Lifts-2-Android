210236069c717d07ad2b4064702dd2f504ad1a6b
Stefan Kendall <stefankendall@gmail.com>
2014-09-10 19:35:59 -0400

    Locked assistance to iap.

--- src/com/stefankendall/BigLifts/views/fto/plan/assistance/FTOAssistanceActivity.java
+++ src/com/stefankendall/BigLifts/views/fto/plan/assistance/FTOAssistanceActivity.java
@@ -3,8 +3,10 @@ package com.stefankendall.BigLifts.views.fto.plan.assistance;
 import android.app.Fragment;
 import android.os.Bundle;
 import com.stefankendall.BigLifts.views.fto.FTOSingleFragmentActivity;
+import com.stefankendall.BigLifts.views.fto.FTOSingleFragmentActivityIapOverlay;
+import com.stefankendall.BigLifts.views.fto.FTOSingleFragmentActivityWithOverlay;
 
-public class FTOAssistanceActivity extends FTOSingleFragmentActivity {
+public class FTOAssistanceActivity extends FTOSingleFragmentActivityIapOverlay {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
