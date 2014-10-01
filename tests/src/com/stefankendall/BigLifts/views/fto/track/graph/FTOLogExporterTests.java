f9db95640cbfb619f5f3e42c86515bfe43756b85
Stefan Kendall <stefankendall@gmail.com>
2014-09-30 22:17:47 -0400

    Added log exporter. Need to add tests.

---
+++ tests/src/com/stefankendall/BigLifts/views/fto/track/graph/FTOLogExporterTests.java
@@ -0,0 +1,49 @@
+package com.stefankendall.BigLifts.views.fto.track.graph;
+
+import com.stefankendall.BigLifts.BLTestCase;
+import junit.framework.Assert;
+
+public class FTOLogExporterTests extends BLTestCase {
+    public void testConvertsWorkoutLogsIntoCsv() {
+        Assert.fail("pending");
+//        JWorkoutLog * workoutLog1 =[[JWorkoutLogStore instance]create];
+//        workoutLog1.name = @ "5/3/1";
+//        workoutLog1.date =[self dateFor:@ "2013-01-23"];
+//
+//        JSetLog * setLog1 =[[JSetLogStore instance]create];
+//        setLog1.weight = N(200);
+//        setLog1.reps = @ 3;
+//        setLog1.name = @ "Deadlift";
+//
+//        JSetLog * setLog2 =[[JSetLogStore instance]create];
+//        setLog2.weight = N(210);
+//        setLog2.reps = @ 2;
+//        setLog2.name = @ "Deadlift";
+//        [workoutLog1.sets addObjectsFromArray:@[setLog1, setLog2]];
+//
+//        JWorkoutLog * workoutLog2 =[[JWorkoutLogStore instance]create];
+//        workoutLog2.name = @ "5/3/1";
+//        workoutLog2.date =[self dateFor:@ "2013-02-01"];
+//
+//        JSetLog * setLog2_1 =[[JSetLogStore instance]create];
+//        setLog2_1.weight = N(100);
+//        setLog2_1.reps = @ 5;
+//        setLog2_1.name = @ "Press";
+//        [workoutLog2 addSet:setLog2_1];
+//
+//        NSMutableString * expected =[@ "name,date,weight,reps,estimated max\n" mutableCopy];
+//        [expected appendString:@ "Press,2/1/13,100,5,116.7\n"];
+//        [expected appendString:@ "Deadlift,1/23/13,210,2,224\n"];
+//
+//        NSString * csv =[[FTOLogExporter new]csv];
+//        STAssertEqualObjects(csv, expected, @ "");
+    }
+
+    public void testDoesNotCrashForNilLiftName() {
+//        JWorkoutLog *workoutLog1 = [[JWorkoutLogStore instance] createWithName:@"5/3/1" date:[NSDate new]];
+//        JSetLog *setLog1 = [[JSetLogStore instance] create];
+//        [workoutLog1 addSet:setLog1];
+//        [[FTOLogExporter new] csv];
+        Assert.fail("pending");
+    }
+}
