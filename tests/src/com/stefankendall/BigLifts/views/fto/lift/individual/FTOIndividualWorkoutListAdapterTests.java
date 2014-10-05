76cb3866d74234ad5d2bd0c25a84645f9fb7b314
Stefan Kendall <stefankendall@gmail.com>
2014-10-04 20:32:16 -0400

    Fixed tests

--- tests/src/com/stefankendall/BigLifts/views/fto/lift/individual/FTOIndividualWorkoutListAdapterTests.java
+++ tests/src/com/stefankendall/BigLifts/views/fto/lift/individual/FTOIndividualWorkoutListAdapterTests.java
@@ -11,12 +11,13 @@ public class FTOIndividualWorkoutListAdapterTests extends BLTestCase {
     public void testBuildsHeadersAndSetsForWarmupAndWorksets() {
         JFTOWorkout jftoWorkout = (JFTOWorkout) JFTOWorkoutStore.instance().first();
         FTOIndividualWorkoutListAdapter adapter = new FTOIndividualWorkoutListAdapter(null, jftoWorkout);
+        int TOOLBAR = 1;
         int WARMUP_HEADER = 1;
         int WARMUP_SETS = 3;
         int WORK_SETS_HEADER = 1;
         int WORK_SETS = 3;
 
-        Assert.assertEquals(adapter.getCount(), WARMUP_HEADER + WARMUP_SETS + WORK_SETS_HEADER + WORK_SETS);
+        Assert.assertEquals(adapter.getCount(), TOOLBAR + WARMUP_HEADER + WARMUP_SETS + WORK_SETS_HEADER + WORK_SETS);
     }
 
     public void testGetsSetRowFromPosition() {
@@ -26,14 +27,15 @@ public class FTOIndividualWorkoutListAdapterTests extends BLTestCase {
 
         FTOIndividualWorkoutListAdapter adapter = new FTOIndividualWorkoutListAdapter(null, jftoWorkout);
         Assert.assertEquals(adapter.setNumberForPosition(0), -1);
-        Assert.assertEquals(adapter.setNumberForPosition(1), 0);
-        Assert.assertEquals(adapter.setNumberForPosition(2), 1);
-        Assert.assertEquals(adapter.setNumberForPosition(3), 2);
-        Assert.assertEquals(adapter.setNumberForPosition(4), -1);
-        Assert.assertEquals(adapter.setNumberForPosition(5), 3);
-        Assert.assertEquals(adapter.setNumberForPosition(6), 4);
-        Assert.assertEquals(adapter.setNumberForPosition(7), 5);
-        Assert.assertEquals(adapter.setNumberForPosition(8), -1);
-        Assert.assertEquals(adapter.setNumberForPosition(9), 6);
+        Assert.assertEquals(adapter.setNumberForPosition(1), -1);
+        Assert.assertEquals(adapter.setNumberForPosition(2), 0);
+        Assert.assertEquals(adapter.setNumberForPosition(3), 1);
+        Assert.assertEquals(adapter.setNumberForPosition(4), 2);
+        Assert.assertEquals(adapter.setNumberForPosition(5), -1);
+        Assert.assertEquals(adapter.setNumberForPosition(6), 3);
+        Assert.assertEquals(adapter.setNumberForPosition(7), 4);
+        Assert.assertEquals(adapter.setNumberForPosition(8), 5);
+        Assert.assertEquals(adapter.setNumberForPosition(9), -1);
+        Assert.assertEquals(adapter.setNumberForPosition(10), 6);
     }
 }
